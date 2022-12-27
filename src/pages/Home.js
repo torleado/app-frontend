0086c295660570100cd245dbb7f3483dd7f27e0f
Sagar <sagarkhanapurkar@gmail.com>
2022-12-27 13:30:51 -0500

    hello

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts', {
+      const response = await fetch('https://torleado.herokuapp.com/api/workouts', {
         headers: {'Authorization': `Bearer ${user.token}`},
       })
       const json = await response.json()
