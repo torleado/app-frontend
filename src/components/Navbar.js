a777607957040f95e29d328eab9f6899771f5146
Sagar <sagarkhanapurkar@gmail.com>
2022-12-27 12:12:07 -0500

    testing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Torleado</h1>
+          <h1>Torleado App</h1>
         </Link>
         <nav>
           {user && (
