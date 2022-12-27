65059bdb7866f77215e446b4b0a6ee40f07a846e
Sagar <sagarkhanapurkar@gmail.com>
2022-12-27 17:48:57 -0500

    added form

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,12 +1,12 @@
 import { useAuthContext } from "../hooks/useAuthContext"
-import WorkoutForm from '../components/WorkoutForm'
+
 import InfluForm from '../components/InfluForm'
 const Add = () => {
 
      const {user} = useAuthContext()
 return(
     <div>
-        <h1>Hello</h1>
+        <h1>Hello {user.email}</h1>
         <InfluForm />
     </div>
 )
